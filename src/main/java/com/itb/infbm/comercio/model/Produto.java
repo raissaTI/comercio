3daf39fd410e509c80708ca185f397e5bbfc5653
Raíssa Botelho <raissabotelho153@gmail.com>
2023-06-23 09:26:53 -0300

    Projeto

--- src/main/java/com/itb/infbm/comercio/model/Produto.java
+++ src/main/java/com/itb/infbm/comercio/model/Produto.java
@@ -11,6 +11,42 @@ public class Produto {
 	// public: Acesso liberado para todas as classes.
 	// private: Acesso liberado apenas para os membros da propria classe.
 	//
-	
+		
+	// Métodos Setter's e Getter's: utilizamos para atribuir e recuperar informações
+	// de um referido atributo "RESPECTIVAMENTE". Para cada atributo teremos o set e o get.
+		
 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
 }
